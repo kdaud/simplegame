fcdbcaa764f19edccb2acb4ae08863280375571c
kdaud <dkakumirizii@gmail.com>
2020-01-12 21:58:25 +0300

    files

---
+++ src/com/software/Coding.java
@@ -0,0 +1,10 @@
+package com.software;
+
+public class Coding {
+    static {
+        System.out.println("God is all with me");
+    }
+    public static  void main(String [] args){
+        
+    }
+}
